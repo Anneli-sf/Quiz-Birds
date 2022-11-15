5161ad4ed4f1e06894db7cf8218039849985e645
Anneli Savchenko <foranneli@gmail.com>
2022-11-16 00:10:38 +0100

    feat: add main page

--- songbird/src/js/main-page.js
+++ songbird/src/js/main-page.js
@@ -1,1 +1,9 @@
 // alert("main page");
+
+const BTN_PLAY = document.querySelector("#btn-play");
+const SCORE_INPUT = document.querySelector(".nav-score");
+
+BTN_PLAY.addEventListener("click", () => {
+SCORE_INPUT.classList.add("visible");
+BTN_PLAY.classList.add("hidden");
+})
